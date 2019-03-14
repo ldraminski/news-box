920d6b2c06aada6c187892cc2d1f7d8468d475b4
Łukasz Dramiński <lukasz.draminski@agora.pl>
2019-03-14 08:10:03 +0100

    [CSS] Add github button

--- webpack.dev.js
+++ webpack.dev.js
@@ -54,7 +54,7 @@ module.exports = {
             ,
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jp(e*)g|svg)$/,
                 use: [
                     {
                         loader: 'url-loader',
--- webpack.prod.js
+++ webpack.prod.js
@@ -62,7 +62,7 @@ module.exports = {
             },
             {
                 // Load all images as base64 encoding if they are smaller than 8192 bytes
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jp(e*)g|svg)$/,
                 use: [
                     {
                         loader: 'url-loader',
